b64eb4532e062eeb8e6a6b81168442602802f6b7
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-21 13:09:24 +0530

    string length method

---
+++ JAVA SCRIPT/string length.js
@@ -0,0 +1,14 @@
+
+// The length property returns the length of a string:
+
+// let num="raghu"
+// let name="chinna"
+// let total=num+name
+// console.log(total)
+// let length=total.length
+// console.log(length)
+
+
+// let x="raghu,rahul,ramesh"
+// console.log(x.length)
+
