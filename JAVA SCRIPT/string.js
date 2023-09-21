2c107deee655180cc8c66da23e77016cb51dda3c
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-21 12:56:39 +0530

    basic string

--- JAVA SCRIPT/string.js
+++ JAVA SCRIPT/string.js
@@ -2,7 +2,24 @@
 // let y="chinna"
 // console.log(x)
 
-let x="raghu"
-let y="chinna"
-let lenght=(x+y).length
-console.log(lenght)
+// let x="raghu"
+// let y="chinna"
+// let lenght=(x+y).length
+// console.log(lenght)
+
+//  e model lo numbers echinapudu length anedhi radhu 
+// let num=10
+// let name=5
+// let total=num+name
+// console.log(total)
+// let length=total.length
+// console.log(length)
+
+
+// above programme ni string lo raste matram manam string length ni teluskovachu
+// let num="raghu"
+// let name="chinna"
+// let total=num+name
+// console.log(total)
+// let length=total.length
+// console.log(length)
