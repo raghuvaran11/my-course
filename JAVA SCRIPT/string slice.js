3c8cb2caad1d69ca417141ab3bb1e035be107195
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-21 13:59:49 +0530

    string slice method

---
+++ JAVA SCRIPT/string slice.js
@@ -0,0 +1,33 @@
+// If you omit the second parameter, the method will slice out the rest of the string:
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7,13);
+// console.log(part)   e mthodel manam echina value lo yem word vundho adhi show chestundhi slice method lo manam 0 nundi count chestam 
+
+// Note
+// JavaScript counts positions from zero.
+
+// First position is 0.
+
+// Second position is 1.
+
+
+// let text = "Apple, Banana, Kiwi"
+// let part = text.slice(text.length-1)
+// console.log(part)
+
+// method 2
+// NOTE: last nundi count cheyadaniki -numbers tho use cheyali eka 
+
+// let text="slice() extracts a part of a string and returns the extracted part in a new string.";
+// let data= text.slice(-10,-1);
+// let length=text.length
+// console.log(length)
+// console.log(data)
+
+
+
+// let text="slice() extracts a part of a string and returns the extracted part in a new string."
+// let data=text.slice(-14,-1)
+// console.log(data.length) e method program lo manam echina output length ni find cheyavchu 
+
