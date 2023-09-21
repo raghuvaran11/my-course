164415b74a119788ad96c40acc4961b4e64dd155
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-21 12:03:27 +0530

    java basic

---
+++ JAVA SCRIPT/basic .js
@@ -0,0 +1,11 @@
+// string and numbers methods 
+
+// var name1="raghu"
+// if (true) {
+//     var name2="chinna"
+//     console.log(name2)
+// }
+// console.log(name1)
+ 
+let name="raghu"
+console.log(name)
