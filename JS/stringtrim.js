269f0d1cb6d98abd956987095c071c1b34e38765
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-24 14:44:09 +0530

    trim method in string

---
+++ JS/stringtrim.js
@@ -0,0 +1,16 @@
+// let x="               hello world            "
+// console.log(x)
+
+// let x="               hello world            "
+// let y=x.trim()
+// console.log(y)
+
+
+
+// let x="               hello world      "
+// let y=x.trimStart()
+// console.log(y)
+
+let x="               hello world      "
+let y=x.trimEnd()
+console.log(y)
