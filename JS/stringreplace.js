0d0c1e428ea4c0279f385cfc7cb353dc4424f37a
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-24 14:05:58 +0530

    replace and replace all methods

---
+++ JS/stringreplace.js
@@ -0,0 +1,45 @@
+// let x="hello good morning"
+// let y=x.replace("morning","evening")
+// console.log(y)
+
+// e method only word matrame replace cheyavachu 
+
+// let x="hello good morning"
+// let y=x.replace("hello","evening")
+// console.log(y)
+
+// REPLACE ALL METHOD
+// e method manam echina paragraph or sentence total ga change cheyvachu 
+
+// let x="hello good morning"
+// let y=x.replaceAll("hello good morning","HAI GOOD evening")
+// console.log(y)
+
+
+// I AND G METHOD IN STRING REPLACE
+
+// G= METHOD LO manaki kavalisina words ni anni okkasare change cheskovachu ex:- raghu ani vunna prathi place lo manam eche inkokka string anedhi replace ithadi dhanine "global" antaru  
+
+
+// let text = "I love cats. Cats are very easy to love. Cats are very popular";
+// // text = text.replaceAll(/Cats/g,"Dogs");
+// text = text.replace(/cats/g,"dogs");
+// console.log(text)
+
+// I METHOD
+
+// e method manam echina string lo  cats anedhi number of times vundhi but e method manam echina value akkada match ithadho akkada manaki kavalsina value anedhi okkasari matrame show ithadhi 
+
+
+let text = "I love cats. Cats are very easy to love. Cats are very popular";
+text = text.replace(/Cats/i,"Dogs");
+text = text.replace(/cats/i,"dogs");
+console.log(text)
+
+
+
+
+
+
+
+
