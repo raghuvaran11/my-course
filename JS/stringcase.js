e50e8b445d63ca10d20e454af6b032882b347e09
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-24 14:27:00 +0530

    touppercase and tolowercase method

---
+++ JS/stringcase.js
@@ -0,0 +1,8 @@
+// let x=("A string is converted to upper case with toUpperCase():")
+// let y= x.toUpperCase(10)
+// console.log(y)
+
+
+let x=("A STRING IS CONVERTED TO UPPER CASE WITH TOUPPERCASE():")
+let y= x.toLowerCase(10)
+console.log(y)
