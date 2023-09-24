222722006456ca238da5a24d87ed4d3bbd69cd12
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-24 20:34:48 +0530

    padding method

---
+++ JS/stringpad.js
@@ -0,0 +1,23 @@
+
+// string startpadding method
+// The padStart() method pads a string from the start.
+// It pads a string with another string (multiple times) until it reaches a given length.
+
+// let text = "5";
+// let padded = text.padStart(4,"0");
+// console.log(padded)
+
+// let text = "5";
+// let padded = text.padStart(4,"*");
+// console.log(padded)
+
+// NOTE: numbers echinapudu first echina number ni string loki convert cheskovali  next paddding ni cheyali 
+
+// let numb = 5;
+// let text = numb.toString();
+// let padded = text.padStart(4,"0");
+// console.log(padded)
+
+let num="10"
+let text= num.padEnd(6,"x")
+console.log(text)
