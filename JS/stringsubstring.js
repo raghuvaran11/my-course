9a45343d511ce0d1d3f5dfeb609cb0e40e08c763
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-24 13:36:19 +0530

    str method

--- JS/stringsubstring.js
+++ JS/stringsubstring.js
@@ -6,11 +6,18 @@
 // substring lo only first nundi last ki matrame try chestam slice method lo laga last nundi try cheyalemu 
 
 
-let world = "raghu,rahul,ramesh,suresh,apple,kiwi"
-let dist = world.substring(12,19)
-console.log(dist,(dist.length))
+// let world = "raghu,rahul,ramesh,suresh,apple,kiwi"
+// let dist = world.substring(12,19)
+// console.log(dist,(dist.length))
 
 
 // let x="HTML is the standard markup language for Web pages"
 // let y=x.substring(10,20)
 // console.log(y)
+
+// substr method
+
+
+let x="HTML is the standard markup language for Web pages"
+let y=x.substr(10,20)
+console.log(y)
