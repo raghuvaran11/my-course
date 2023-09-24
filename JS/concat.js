4db4da8894525896618d62465acfc46ca0459df7
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-24 14:34:30 +0530

    concat method

---
+++ JS/concat.js
@@ -0,0 +1,14 @@
+// let x= "raghu"
+// let y="chinna"
+// let z=x.concat(" ",y)
+// console.log(z)
+
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1.concat(" ", text2);
+// console.log(text3)
+
+
+text = "Hello" + " " + "World!";
+text = "Hello".concat(" ", "World!");
+console.log(text)
