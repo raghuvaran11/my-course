8d8029c6b934d71eb22dcf575da359eae1f43b71
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-28 15:03:52 +0530

    array method

---
+++ JS/array.js
@@ -0,0 +1,32 @@
+// An array is a special variable, which can hold more than one value: 
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars)
+ 
+// empty array lo elements add cheyadam 
+
+// const cars = [];
+// cars[0]= "Saab";
+// cars[1]= "Volvo";
+// cars[2]= "BMW";
+// console.log(cars)
+
+// array ala echina okkate output vastadi
+// const cars = [
+//     "Saab",
+//     "Volvo",
+//     "BMW"
+//   ];
+//   console.log(cars)
+
+// echina array nundi element ni single ga tiskovadam
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// let car = cars[1]
+// console.log(car)
+
+// echina array nundi element ni change cheyadam
+// const cars = ["Saab", "Volvo", "BMW"];
+// cars[0] = "shift"
+// console.log(cars)
+
