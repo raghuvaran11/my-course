e239584ae709f34c8159dc9fa3e3756d2066f9f4
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-29 17:59:18 +0530

    array shift method

---
+++ JS/arrayshift.js
@@ -0,0 +1,8 @@
+// The shift() method removes the first array element and "shifts" all other elements to a lower index.
+
+// let remove= ["push","pop","unshift","shift","delete"]
+// remove.shift()
+// console.log(remove)
+
+let remove= ["push","pop","unshift","shift","delete"]
+console.log(remove.shift())
