249a7164c6976ee171304c49c8d03e5038e85732
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-29 12:07:46 +0530

    array pop method

---
+++ JS/arraypop.js
@@ -0,0 +1,33 @@
+//  pop method anedhi last lo vunde elmement ni delete chestundhi 
+
+// let string =["string","push","pop","shift","unshift","array","raghu","ramesh","vinith","vinod"]
+// string.pop()
+// console.log(string)
+
+// 1... delete ina element ni show cheyadaniki 
+
+// let string =["string","push","pop","shift","unshift","array","raghu","ramesh","vinith","vinod"]
+// let remove= string.pop()
+// console.log(remove)
+
+//  2... remove chesina element and remainning string anedhi show cheyali output lo
+
+// let string =["string","push","pop","shift","unshift","array","raghu","ramesh","vinith","vinod"]
+// console.log(string.pop(),string.slice(0))
+
+// 3... add and remove at a time cheyadam 
+
+let string =["string","push","pop","shift","unshift","array","raghu","ramesh","vinith","vinod"]
+console.log(string.push("kiwi"),string.pop())
+console.log(string.pop(),string.push("kiwi"))
+
+
+
+
+
+
+
+
+
+
+
