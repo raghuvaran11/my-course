20b06542d676c1df556e5545f7942efa1b4c44a1
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-29 11:21:21 +0530

    array push method

---
+++ JS/arraypush.js
@@ -0,0 +1,43 @@
+// 1...empty array lo elements add cheyadam
+
+// let array=[]
+// array.push ("raghu","ramesh","vinith","vinod")
+// console.log(array)
+
+//2... echina array elements lo replace cheyadam method
+
+// let array=["raghu","ramesh","vinith","vinod"]
+// array[2]="naresh"
+// console.log(array)
+
+// 3... echina array lo index loni value lo letter ni findout cheyadam kosam 
+
+// let array=["raghu","ramesh","vinith","vinod"]
+// console.log(array[2][3])
+
+// 4 .... echina arrey length find out cheyadam kosam
+
+// let string =["string","push","pop","shift","unshift","array","raghu","ramesh","vinith","vinod"]
+// let size = string.length
+// console.log(size)
+
+//  5.... manam echina array list loni okka element length ni findout cheyadam
+
+// let string =["string","push","pop","shift","unshift","array","raghu","ramesh","vinith","vinod"]
+// console.log(string[5].length)
+
+//  6....array to string covert method
+
+// let array =["string","push","pop","shift","unshift","array","raghu","ramesh","vinith","vinod"]
+// let output=array.toString()
+// console.log(output)
+
+
+
+
+
+
+
+
+
+
