0dcb25084f94404397033d0af226f7b39947088e
raghuvaran10 <raghu.mr199@gmail.com>
2023-10-03 11:57:54 +0530

    function method

---
+++ JS/function.js
@@ -0,0 +1,50 @@
+// this function is called arrow function      
+
+// const sum1 =(a,b) =>{
+//     console.log(a,b)
+// }
+// sum1("raghu"," 1")
+
+// const num1= (a,b,c)=>{
+//     console.log(a+b+c)
+// }
+// num1(1,2,3) bodmas
+
+// const num1= (a,b,c)=>{
+//     console.log(a+b-c)
+// }
+// num1(1,2,3)
+
+
+// const num1= (a,b,c)=>{
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// }
+// num1(1,2,3)
+
+// claculator programme
+
+
+// const output=(a,b,operator)=>{
+// if (operator=="+"){
+//     console.log(a+b)
+// } else if(operator=="-"){
+//     console.log(a-b)
+// } else if(operator=="/"){
+//     console.log(a/b)
+// } else if(operator=="*"){
+//     console.log(a*b)
+// }else if(operator=="%"){
+//     console.log(a%b)
+// }else {
+//     console.log("no results")
+// }
+// }
+// output(1,2,"+")
+// output(10,5,"-")
+// output(17,3,"/")
+// output(1,2,"*")
+// output(100,13,"%")
