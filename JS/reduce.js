b9fabffa9f45fd1977ef07ff8971ea554209722b
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-11-02 11:51:53 +0530

    reduce

--- JS/reduce.js
+++ JS/reduce.js
@@ -1,8 +1,8 @@
-const numbers =[ 1, 2, 3, 4, 5, 6, 7 ];
-let sum=0
-numbers.map((item)=>{
-    sum +=item
-})
+// const numbers =[ 1, 2, 3, 4, 5, 6, 7 ];
+// let sum=0
+// numbers.map((item)=>{
+//     sum +=item
+// })
 
 // let accumulator=0 ;
 // numbers.map((currentvalue)=>{
@@ -20,3 +20,34 @@ numbers.map((item)=>{
 // console.log(sum);
 
 
+// 1.find the max value in the array
+
+// const numbers1=[12, 15, 31, 25, 30]
+// const max= numbers1.reduce((accumulator,CurrentValue) => {
+// return accumulator>CurrentValue?accumulator:CurrentValue
+// },-Infinity);
+// console.log(max)
+
+// 2. find the minimum value in the array 
+
+// const numbers=[12, 15, 31, 25, 30]
+// const minimum = numbers.reduce((accumulator,CurrentValue) => {
+//     return accumulator<CurrentValue?accumulator:CurrentValue
+//     },Infinity);
+//     console.log(minimum)
+
+
+// 3. concating an array of strings
+// const numbers=["hello", " ", "world!"]
+// const minimum = numbers.reduce((accumulator,CurrentValue) => {
+//     return accumulator+CurrentValue
+// }," ");
+//     console.log(minimum)
+
+// 4. flattening an array of arrays
+
+const flat=[[1, 2],[3, 4],[5, 6]]
+const mix=flat.reduce((accumulator,CurrentValue) => {
+        return accumulator.concat(CurrentValue)
+},[])
+console.log(mix)
