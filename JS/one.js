c5d05811c296328d16df802295320c5afb88c637
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-15 22:36:47 +0530

    length

--- JS/one.js
+++ JS/one.js
@@ -1,3 +1,6 @@
-let string = "raghu"
-let string1 ="varan"
-console.log(string+string1)
+// let string = "raghu"
+// let string1 ="varan"
+// console.log(string+string1)
+
+// let string = "console"
+// console.log(string.length)
