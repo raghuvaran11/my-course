780e286a060f557e7d4bc5324f801c82ea5090b7
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-15 23:41:00 +0530

    length

--- JS/one.js
+++ JS/one.js
@@ -5,7 +5,32 @@
 // let string = "console"
 // console.log(string.length)
 
-let string ="javascript "
-let string1 =string.length
-console.log(string)
-console.log(string1)
+// let string ="javascript "
+// let string1 =string.length
+// console.log(string)
+// console.log(string1+" "+string)
+
+
+// let string ="raghu"
+// let length=string.length
+// // console.log(string1)
+// let output =length.padStart(3,"*")
+// console.log(output)
+
+// let string="raghuvaran"
+// let output =string.padEnd(9,"*")
+// console.log(output)
+
+// let email="robin_singh@example.com"
+// let first_name=email.slice(0,5)
+// let pad=first_name.padEnd(12,".")
+// let remaining=email.substring(11)
+// let output=pad.concat(remaining)
+// console.log(output)
+
+
+let email =raghuvaran@gmail.com
+let first_name= email.slice(0,5)
+let second =first_name.padEnd(9,"*")
+let output =first_name.concat(second)
+console.log(output)
