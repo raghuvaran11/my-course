253b537e0cb840261679c9134639ae291a15c596
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-16 20:35:56 +0530

    string1-method

--- JS_que/string1.js
+++ JS_que/string1.js
@@ -5,6 +5,44 @@
 // let output = typeof((string))
 // console.log(output)
 
-let number = 25
-let output = typeof(number)
-console.log(output)
+// let number = 25
+// let output = typeof(number)
+// console.log(output)
+
+// let string ="raghu"
+// let number = 26
+// let add = string.concat(number)
+// console.log(add)
+// console.log(typeof(add))
+// const length= add.length
+// console.log(length)
+
+
+// let string= ("raghu","ramesh")
+// let string1= ("suresh","sunitha")
+// let add = string.concat(string1)
+// console.log(add)
+
+
+
+
+// 1. How do you find the length of a string in JavaScript?
+
+// let str =" How do you find the length of a string in JavaScript"
+// let op =str.length
+// console.log(op)
+// console.log(str.length,str)
+
+
+// 2. What method is used to convert a string to lowercase in JavaScript?
+// let str="WHAT METHOD IS USED TO CONVET A STRING TO LOWERCASE IN JAVASCRIPT?"
+// let op =str.toLowerCase()
+// console.log(op)
+// let op1=str.toLowerCase().length
+// console.log(op1)
+
+let str1 ="WHAT METHOD IS USED TO CONVET A STRING TO LOWERCASE IN JAVASCRIPT"
+let str2=str1.slice(0,10)
+console.log(str2)
+let op3 = str1[10].toLowerCase()+str1.slice(11)
+console.log(str2+op3)
