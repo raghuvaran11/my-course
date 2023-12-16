6826c0f5b5eeb9e864f44c8d1bdc3f5c8adccf5b
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-16 20:03:33 +0530

    string-method

---
+++ JS_que/string1.js
@@ -0,0 +1,10 @@
+// let string ="this is a string"
+// console.log(typeof(string))
+
+// let string = "this is a string method"
+// let output = typeof((string))
+// console.log(output)
+
+let number = 25
+let output = typeof(number)
+console.log(output)
