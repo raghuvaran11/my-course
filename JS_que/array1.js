31e6e5446dca8b28232bf5d97a7bd6c09f956b46
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-17 17:58:49 +0530

    array

--- JS_que/array1.js
+++ JS_que/array1.js
@@ -1,6 +1,17 @@
 // factorioal method
-let number=5
-let str=1
-for (let i=1; i<=number;i++){
-    str *=i
-} console.log(str)
+// let number=5
+// let str=1
+// for (let i=1; i<=number;i++){
+//     str *=i
+// } console.log(str)
+
+
+// multification method 
+let num = 5
+for (let i=0; i<=num;i++){
+    let multi=
+}
+
+
+
+
