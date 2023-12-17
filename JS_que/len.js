aace5306fd1757b3108bba6888ebf6bef3f8efee
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-17 18:20:09 +0530

    pattern

--- JS_que/len.js
+++ JS_que/len.js
@@ -2,10 +2,21 @@
 // let op =str.split('').reverse('').join('')
 // console.log(op)
 
-let a=0,b=1;
+// let a=0,b=1;
 // for (let i=0;i<=10;i++) {
 //     let temp=a+b ;
 //     a=b;
 //     b=temp;
 // console.log(temp)
 // }
+
+
+let row= 5
+for (let i = 1; i <= row; i++) {
+    let pattern = "";
+    for (let j = 1; j <= i; j++) {
+      pattern += "*";
+    }
+    console.log(pattern);
+  }
+  
