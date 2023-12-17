0156a58f9eef39be441602ffbda3bcb8e4adc1c4
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-17 18:15:00 +0530

    array

--- JS_que/len.js
+++ JS_que/len.js
@@ -1,3 +1,11 @@
 // let str ="hello"
 // let op =str.split('').reverse('').join('')
 // console.log(op)
+
+let a=0,b=1;
+// for (let i=0;i<=10;i++) {
+//     let temp=a+b ;
+//     a=b;
+//     b=temp;
+// console.log(temp)
+// }
