45a75ba9b6db57a6f0b9e08d4cf40bdbdd5ec9ba
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-19 19:42:35 +0530

    for loop

--- JS_que/for.js
+++ JS_que/for.js
@@ -1,5 +1,14 @@
+// let n=100
+// let sum=0
+// for (let i=1;i<=n;i++){
+//     sum += i
+// }console.log(sum)
+
 let n=100
 let sum=0
-for (let i=1;i<=n;i++){
-    sum += i
-}console.log(sum)
+for (let i=0;i<=n;i++) {
+    if(i % 2 ==0){
+        sum += i
+    }
+}
+console.log(sum)
