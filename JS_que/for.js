81b8063e2a38a194e2756f5490f58aaa555d0a38
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-20 11:43:41 +0530

    padmethod

--- JS_que/for.js
+++ JS_que/for.js
@@ -14,10 +14,24 @@
 // console.log(sum)
 
 
-let n=100
-let sum=0
-for (let i=0;i<=n;i++){
-    if(i % 2 !=0){
-        sum +=i
-    }
-} console.log(sum)
+// let n=100
+// let sum=0
+// for (let i=0;i<=n;i++){
+//     if(i % 2 !=0){
+//         sum +=i
+//     }
+// } console.log(sum)
+
+// let name= "raghuvaran"
+// let halfname=name.slice(-5)
+// let markname=halfname.padStart(name.length,"*")
+// console.log(markname)
+
+
+let name= "raghuvaranmanthena"
+let halfname=name.slice(0,5)
+console.log(halfname)
+let middlename =name.slice(5,10)
+console.log(middlename)
+let markname =middlename.padEnd(3,"*")
+console.log(markname)
