2b7e546216a2c66aec6e48872214f91f4cf392da
raghuvaran11 <raghuvaranmanthena@gmail.com>
2023-12-26 22:37:41 +0530

    questions

--- JS_que/ques.js
+++ JS_que/ques.js
@@ -241,7 +241,19 @@
 // **Question 5: Check for Palindrome**
 // Write a function `isPalindrome` that takes a string as input and returns `true` if it's a
 // palindrome (reads the same forwards and backward), and `false` otherwise.
-function checkpalindrome(){
-   let input = "level"
-   let reverse=
-}
+
+
+// function checkpalindrome(){
+//    let input = "level"
+//    let reverse=
+// }
+
+
+11. Write a function `isAnagram` that takes two strings as input and returns `true` if they
+are anagrams (contain the same characters in a different order), and `false` otherwise.
+**Example:**
+```javascript
+const input1 = "listen";
+const input2 = "silent";
+const output = isAnagram(input1, input2);
+console.log(output); // true
