3f6da439121952c48d7c748bd22819ddf8fc4244
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-01 17:53:14 +0530

    intersection

--- JS_que/ques.js
+++ JS_que/ques.js
@@ -436,3 +436,13 @@
 
 //    }
 // }
+
+// 21. Write a function `findUnique` that takes two arrays as input and returns an array
+// containing elements that are unique to each input array (i.e., not common between
+// them).
+// **Example:**
+// ```javascript
+// const inputArray1 = [1, 2, 3, 4, 5];
+// const inputArray2 = [3, 4, 5, 6, 7];
+// const output = findUnique(inputArray1, inputArray2);
+// console.log(output); // [1, 2, 6, 7]
