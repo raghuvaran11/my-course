69b51ab5a2de9134a45dc4d3f6c42c65d9621cf8
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-03 12:35:38 +0530

    natural number sum

---
+++ JS_que/inter.js
@@ -0,0 +1,10 @@
+// . Sum of Natural Numbers:**
+// Write a program to calculate the sum of natural numbers from 1 to `n` using a `for` loop.
+
+
+let n=5
+let sum =0
+for (let i=0;i<=n;i++){
+    sum += i;
+}
+console.log(sum)
