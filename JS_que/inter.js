34c9cbd66edd81fb6954b2a82457fdbb7ebe250a
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-03 12:46:25 +0530

    fibonacci

--- JS_que/inter.js
+++ JS_que/inter.js
@@ -13,9 +13,31 @@
 // **2. Factorial of a Number:**
 // Write a program to calculate the factorial of a given number `n` using a `for` loop.
 
-let str = 5
-let mul=1
-for (let i=1;i<=str;i++){
-    mul *=i
+// let str = 5
+// let mul=1
+// for (let i=1;i<=str;i++){
+//     mul *=i
+// }
+// console.log(mul)
+
+
+// **3. Fibonacci Sequence:**
+// Generate and print the first `n` terms of the Fibonacci sequence using a `for` loop.
+
+// let a=0,b=1;
+// let n=10
+// for (let i=0;i<=n;i++){
+//     let temp=a+b;
+//     a=b;
+//     b=temp;
+//     console.log(temp)
+// }
+
+let a=0,b=1
+let n=5
+for (let i=0;i<=n;i++) {
+    let temp =a+b
+    a=b
+    b=temp
+    console.log(temp)
 }
-console.log(mul)
