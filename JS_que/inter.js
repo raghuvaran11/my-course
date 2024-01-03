02949b73b465989780e5687f2d856fb757347258
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-03 12:41:18 +0530

    factorial method

--- JS_que/inter.js
+++ JS_que/inter.js
@@ -2,9 +2,20 @@
 // Write a program to calculate the sum of natural numbers from 1 to `n` using a `for` loop.
 
 
-let n=5
-let sum =0
-for (let i=0;i<=n;i++){
-    sum += i;
+// let n=5
+// let sum =0
+// for (let i=0;i<=n;i++){
+//     sum += i;
+// }
+// console.log(sum)
+
+
+// **2. Factorial of a Number:**
+// Write a program to calculate the factorial of a given number `n` using a `for` loop.
+
+let str = 5
+let mul=1
+for (let i=1;i<=str;i++){
+    mul *=i
 }
-console.log(sum)
+console.log(mul)
