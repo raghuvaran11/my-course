65a291d42c859a5ce10411a774d491764930b48f
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-05 12:33:16 +0530

    number+string

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -5,7 +5,13 @@
 // console.log(op)
 
 
-const number =10
-console.log(typeof(number))
-const op= typeof(number)
+// const number =10
+// console.log(typeof(number))
+// const op= typeof(number)
+// console.log(op)
+
+const string = "this is string"
+const number= 10
+const op =string+number
 console.log(op)
+console.log(typeof(op))
