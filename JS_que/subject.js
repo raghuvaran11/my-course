cce2373cbd986847fca086c5460c407e6dbe61b2
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-05 12:30:27 +0530

    string typeof

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -1,5 +1,11 @@
 // total string methods
-const string = "total string methods"
-console.log(typeof(string))
-const op = typeof(string)
+// const string = "total string methods"
+// console.log(typeof(string))
+// const op = typeof(string)
+// console.log(op)
+
+
+const number =10
+console.log(typeof(number))
+const op= typeof(number)
 console.log(op)
