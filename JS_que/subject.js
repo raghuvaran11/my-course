c68bcb959fd0ae53f5335a2a8e282a9c678311ce
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-05 13:07:39 +0530

    oject method

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -1,14 +1,18 @@
 // total string methods
+
 // const string = "total string methods"
 // console.log(typeof(string))
 // const op = typeof(string)
 // console.log(op)
 
+// NUMBER METHOD
 
 // const number =10
 // console.log(typeof(number))
 // const op= typeof(number)
 // console.log(op)
+
+// STRING AND NUMBER METHOD
 
 // const string = "this is string"
 // const number= 10
@@ -16,13 +20,29 @@
 // console.log(op)
 // console.log(typeof(op))
 
+// STRING LENGTH METHOD
 
 // const string= "this is a string length"
 // console.log(string)
 // console.log(string.length)
 
-let text = "We are the so-called \"Vikings\" from the north."
-console.log(text)
+// QUOTES METHODS
 
-let text1 = 'We are the so-called "Vikings" from the north.'
-console.log(text1)
+// let text = "We are the so-called \"Vikings\" from the north."
+// console.log(text)
+
+// let text1 = 'We are the so-called "Vikings" from the north.'
+// console.log(text1)
+
+// let text = "The character \\ is called backslash."
+// console.log(text)
+
+// let y = new String("John");
+// console.log(typeof(y))
+
+
+let x = "string"
+let y = new String("John");
+console.log(typeof(x))
+console.log(typeof(y))
+
