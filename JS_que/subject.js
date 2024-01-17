e3907ff3c754aa9a482a802d89b78aa996537d57
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-17 12:05:05 +0530

    array flat

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -494,6 +494,10 @@
 
 
 // JavaScript Array flat()...................................................
-const myArr = [[1,2],[3,4],[5,6]];
+// const myArr = [[1,2],[3,4],[5,6]];
+// const newArr = myArr.flat();
+// console.log(newArr)
+
+const myArr = [[1,2,3],[3,4,5],[5,6,7]];
 const newArr = myArr.flat();
 console.log(newArr)
