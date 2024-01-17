4b504ee095978bb0743ba5ed865d9f66942e01c4
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-17 11:59:25 +0530

    array method

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -486,3 +486,8 @@
 // const fruits = ["Banana", "Orange", "Apple", "Mango","Kiwi"];
 // fruits.copyWithin(2, 0,2);
 // console.log(fruits)
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango","Kiwi"];
+fruits.copyWithin(2, 0);
+console.log(fruits)
