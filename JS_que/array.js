c305c6cee84643d91032cabfa79141447be10ea8
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-20 22:30:52 +0530

    array

--- JS_que/array.js
+++ JS_que/array.js
@@ -75,3 +75,12 @@
 //     b=temp;
 // console.log(temp)
 // }
+// Generate and print the first `n` terms of the Fibonacci sequence using a `for` loop.
+
+// let a=0,b=1;
+// for (let i=0;i<=10;i++) {
+//     let temp=a+b ;
+//     a=b;
+//     b=temp;
+// console.log(temp)
+// }
