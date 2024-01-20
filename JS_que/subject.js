ac4f6deb8d66a968461805014ba2a27137e995b2
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-20 22:29:39 +0530

    array

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -524,6 +524,6 @@
 // const op = string.slice(0,1)
 // console.log(op)
 
-const string = ["Jan", "Feb", "Mar", "Apr"]
-const op = string.slice(1,3)
-console.log(op)
+// const string = ["Jan", "Feb", "Mar", "Apr"]
+// const op = string.slice(1,3)
+// console.log(op)
