83536baa64cc2e68de360951504bcd64d45b53aa
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-20 22:37:02 +0530

    raghu

--- JS_que/array.js
+++ JS_que/array.js
@@ -101,3 +101,20 @@ c
 //         sum +=i
 //     }
 // }console.log(sum)
+// EVEN NUMBERS COUNT--- METHOD
+
+// let number= 100
+// let sum =0
+// for(let i=1;i<=100;i++) {
+//     if(i%2==0) {
+//     sum +=i}
+// } console.log(sum)
+
+// ODD NUMBERS COUNT ---METHOD 
+// let number =100 
+// let sum =0 
+// for(let i=1;i<=number;i++) {
+//     if(i % 2 !=0){
+//         sum +=i
+//     }
+// }console.log(sum)
