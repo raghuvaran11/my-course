cc65ff103f39479a53412eadc4cdba0230d48010
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-24 22:13:17 +0530

    age allowed

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -712,9 +712,9 @@
 // 13)Age Eligibility
 // Write a function checkAge that takes an age as input. The function should return "Allowed" if the
 // age is between 18 and 65 (inclusive), and "Not allowed" otherwise.
-let age=15
-if (age>=18 && age<=65){
-    console.log("allowed")
-} else {
-    console.log("not-allowed")
-}
+// let age=15
+// if (age>=18 && age<=65){
+//     console.log("allowed")
+// } else {
+//     console.log("not-allowed")
+// }
