c166ffcb32bda649d7725eb550c5ca69209e630c
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-01-25 20:23:35 +0530

    age allowed

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -761,3 +761,18 @@
 // }
 
 
+// 17) Adding and Removing Elements
+// Create an array called `fruits` with initial values ["apple", "banana"]. Using the appropriate array
+// methods, perform the following actions:
+// - Add "orange" to the end of the array.
+// - Remove the last element from the array.
+// - Add "grape" to the beginning of the array.
+// - Remove the first element from the array.
+// Print the final array after each step.
+
+// const fruits=["apple","banana"]
+// let add= fruits.push("orange")
+// fruits.pop()
+// fruits.unshift("grape")
+// fruits.shift()
+// console.log(fruits)
