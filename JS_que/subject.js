1ce72efe0e6c4bde197157a8a17f8841e73eed0b
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-02-09 15:31:10 +0530

    functions ques

--- JS_que/subject.js
+++ JS_que/subject.js
@@ -1316,9 +1316,21 @@
 // **2. Factorial of a Number:**
 // Write a program to calculate the factorial of a given number `n` using a `for` loop.
 
-let n= 10
-let factorial = 1
-for(let i=1;i<=n;i++){
-    factorial *= i
+// let n= 10
+// let factorial = 1
+// for(let i=1;i<=n;i++){
+//     factorial *= i
+// }
+//  console.log(factorial)
+
+// *3. Fibonacci Sequence:**
+// Generate and print the first `n` terms of the Fibonacci sequence using a `for` loop.
+
+let a=0,b=1
+let n=10
+for (let i=0;i<=n;i++){
+    let temp=a+b
+    a=b
+    b=temp
+    console.log(temp)
 }
- console.log(factorial)
