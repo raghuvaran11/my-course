ec61a972e89c790447170157b1a5d8fe86aa36c2
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-02-20 10:33:12 +0530

    spreadoperator

---
+++ spread_operate/spread.js
@@ -0,0 +1,5 @@
+const arr1=[1,2,3]
+const arr2=[4,5,6]
+console.log(arr1)
+console.log(...arr1,...arr2)
+// console.log("spread operate",...arr1)   spread operte anedhi array nundi spread ithundhi ex:[1,2,3] manaku output 1,2,3 vastundhi
