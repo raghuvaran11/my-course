2222d76499369ed76a43ac8a71b29f2ecaec5261
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-02-27 21:26:28 +0530

    factorial of given number

--- JS_que/ques.js
+++ JS_que/ques.js
@@ -621,12 +621,12 @@
 // console.log(string.slice(2))
 
 // **1. Sum of Natural Numbers:**
-let number=10;
-let sum =0;
-for (let i=0;i<=number;i++){
-   sum +=i;
-}
-console.log(sum)
+// let number=10;
+// let sum =0;
+// for (let i=0;i<=number;i++){
+//    sum +=i;
+// }
+// console.log(sum)
 
 // let n=5
 // let sum =0
@@ -634,3 +634,12 @@ console.log(sum)
 //     sum += i;
 // }
 // console.log(sum)
+
+
+// **2. Factorial of a Number:**
+let number=5
+let factorial=1
+for(let i=1;i<=number;i++){
+   factorial *= i
+}
+console.log(factorial)
