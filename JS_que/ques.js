f12e27175ada813aa6ade37931e42031d18b4979
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-02-28 23:16:52 +0530

    prime numben

--- JS_que/ques.js
+++ JS_que/ques.js
@@ -723,15 +723,26 @@
 //    }console.log(pattern)
 // }
 
-function prime(n) {
-    if (n<2)
-    return '${n} is not a prime number'
-    for (let i = 2; i<n; i++){
-        if( n % i === 0) {
-            return `${n} is not a Prime Number`
+// function prime(n) {
+//     if (n<2)
+//     return '${n} is not a prime number'
+//     for (let i = 2; i<n; i++){
+//         if( n % i === 0) {
+//             return `${n} is not a Prime Number`
 
-        }
-    }
-    return `${n} is a Prime Number`
+//         }
+//     }
+//     return `${n} is a Prime Number`
+// }
+// console.log(prime(9))
+
+function prime(n) {
+   if(n<2)
+   return`${n} is not a prime number`
+for (let i =2;i<n;i++){
+   if(n%i===0){
+      return `${n} is not a prime number`
+   }
+}return `${n} is a Prime Number`
 }
 console.log(prime(9))
