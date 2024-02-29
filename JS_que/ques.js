bf5a70f231a2bffa8a85e582a5365bafa6af039a
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-02-29 23:11:47 +0530

    print pattern

--- JS_que/ques.js
+++ JS_que/ques.js
@@ -760,3 +760,28 @@
 //     }
 // } 
 // console.log(largest)
+
+// print pattern
+
+// let row =5 
+// for(let i=1;i<=row;i++){
+//    let pattern=""
+//    for (let j=1;j<=i;j++){
+//       pattern += "*"
+//    }console.log(pattern)
+// }
+
+// let row=5
+// for(let i=1;i<=row;i++){
+//    let pattern=""
+//    for(let j=1;j<=i;j++){
+//       pattern+="*"
+//    }console.log(pattern)
+// }
+let row=5
+for(let i=1;i<=row;i++){
+   let pattern=""
+   for(let j=1;j<=i;j++){
+      pattern += "*"
+   }console.log(pattern)
+}
