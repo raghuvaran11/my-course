73bb3801267a0d5ff9afea348bf2d3d3e4a4cf62
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-03-01 11:36:17 +0530

    factorial of natural numbers

--- JS_que/jsques.js
+++ JS_que/jsques.js
@@ -1,8 +1,16 @@
 // **1. Sum of Natural Numbers:**
 
+// let number=5
+// let sum=0
+// for(let i=0;i<=number;i++){
+//     sum +=i
+// }
+// console.log(sum)
+
+// **2. Factorial of a Number:**
+
 let number=5
-let sum=0
-for(let i=0;i<=number;i++){
-    sum +=i
-}
-console.log(sum)
+let factorial=1
+for(let i=1;i<=number;i++){
+    factorial *= i
+} console.log(factorial)
