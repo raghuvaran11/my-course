7898adcbfa2ef15476b2de58c578fa9ad9ccc7fa
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-03-01 11:37:56 +0530

    fibonacci

--- JS_que/jsques.js
+++ JS_que/jsques.js
@@ -9,8 +9,8 @@
 
 // **2. Factorial of a Number:**
 
-let number=5
-let factorial=1
-for(let i=1;i<=number;i++){
-    factorial *= i
-} console.log(factorial)
+// let number=5
+// let factorial=1
+// for(let i=1;i<=number;i++){
+//     factorial *= i
+// } console.log(factorial)
