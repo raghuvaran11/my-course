1d7ccb018d1acc34684a4a6994148cb3326adfba
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-03-01 11:39:03 +0530

    reverse string

--- JS_que/jsques.js
+++ JS_que/jsques.js
@@ -14,3 +14,21 @@
 // for(let i=1;i<=number;i++){
 //     factorial *= i
 // } console.log(factorial)
+
+// **3. Fibonacci Sequence:**
+// let a=0,b=1;
+// let n=10
+// for (let i=0;i<=n;i++){
+//     let temp=a+b;
+//     a=b;
+//     b=temp;
+//     console.log(temp)
+// }
+
+// **4. Reverse a String:**
+// let string="hello world"
+// let reverse=""
+// for(let i=string.length-1;i>=0;i--){
+//    reverse+=string[i]
+// }
+// console.log(reverse)
