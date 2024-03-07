0fd19346704ba5e047c906069fd4763406a3e07c
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-03-07 08:44:14 +0530

    merge two sorted  Arrays

--- JS_que/jsques.js
+++ JS_que/jsques.js
@@ -1,4 +1,4 @@
-// ** Sum of Natural Numbers:**
+// ** Sum of Natural Numbers:**...............................
 
 // let number=5
 // let sum=0
@@ -7,7 +7,7 @@
 // }
 // console.log(sum)
 
-// **  Factorial of a Number:**
+// **  Factorial of a Number:**.....................
 
 // let number=5
 // let factorial=1
@@ -15,7 +15,7 @@
 //     factorial *= i
 // } console.log(factorial)
 
-// ** Fibonacci Sequence:**
+// ** Fibonacci Sequence:**...................................
 
 // let a=0,b=1;
 // let n=10
@@ -26,7 +26,7 @@
 //     console.log(temp)
 // }
 
-// ** Reverse a String:**
+// ** Reverse a String:**...............................
 
 // let string="hello world"
 // let reverse=""
@@ -35,7 +35,7 @@
 // }
 // console.log(reverse)
 
-// ** Reverse an Array:**
+// ** Reverse an Array:**......................................
 
 //     let array=[1,2,3,4,5]
 // let reverseArray=[];
@@ -44,7 +44,7 @@
 // }
 // console.log(reverseArray)
 
-// ** Print Pattern:**
+// ** Print Pattern:**...........................
 
 // let row=5
 // for (let i=1;i<=row;i++){
@@ -55,7 +55,7 @@
 //     console.log(pattern)
 // }
 
-// ** largest number:**
+// ** largest number:**...............................
 
 
     // function findLargestNumber(numbers) {
@@ -79,7 +79,7 @@
     //   console.log(findLargestNumber(numbersList));
       
 
-    // ** smallest number:**
+    // ** smallest number:**..................................
 
     // function findSmallestValue(numbers) {
     //     if (numbers.length === 0) {
@@ -102,7 +102,7 @@
     // console.log(findSmallestValue(numbersList));
 
 
-// ** Find the Maximum Number in an Array**
+// ** Find the Maximum Number in an Array**...............................
 
 //    let array= [5,6,7,9,10,2,12]
 //    let max = array[0]
@@ -114,7 +114,7 @@
 //    }
 //    console.log(max)
 
-// **  find minimum value of array **
+// **  find minimum value of array **.........................
 
 // let array = [2,1,3,5,9,8,7,0]
 // let min= array[0]
@@ -126,7 +126,7 @@
 // console.log (min)
 
 
-// **  countdigit of number  **
+// **  countdigit of number  **................................
 
 // function countDigits(number) 
 // Convert the number to a string to easily get its length
@@ -140,7 +140,7 @@
 //     num = -9876;
 //     console.log("Number of digits in " + num + ": " + countDigits(num));
 
-// **  Count the Occurrences of a Character in a String**
+// **  Count the Occurrences of a Character in a String**..........................................
 
 // function countChar(inputString, char) {
 //     let count = 0;
@@ -158,7 +158,7 @@
 // const result = countChar(inputStr, characterToCount);
 // console.log(`The character '${characterToCount}' appears ${result} times in the string '${inputStr}'.`);
 
-// ** Average value of the numbers **
+// ** Average value of the numbers **.......................
 
 // function findAverage(numbers) {
 //     if (numbers.length === 0) {
@@ -176,22 +176,33 @@
 // const result = findAverage(numbersArray);
 // console.log(`The average of the numbers is: ${result}`);
 
-// **  Remove Duplicates from an Array  **
+// **  Remove Duplicates from an Array  **.....................
 
-function removeDuplicates(numbers) {
-    // Use a Set to store unique values
-    let uniqueNumbers = new Set(numbers);
+// function removeDuplicates(numbers) {
+//     // Use a Set to store unique values
+//     let uniqueNumbers = new Set(numbers);
   
-    // Convert the Set back to an array
-    let uniqueArray = Array.from(uniqueNumbers);
+//     // Convert the Set back to an array
+//     let uniqueArray = Array.from(uniqueNumbers);
   
-    return uniqueArray;
-  }
+//     return uniqueArray;
+//   }
   
-  // Example usage:
-  let inputArray = [1, 2, 2, 3, 4, 4, 5];
-  let resultArray = removeDuplicates(inputArray);
+//   // Example usage:
+//   let inputArray = [1, 2, 2, 3, 4, 4, 5];
+//   let resultArray = removeDuplicates(inputArray);
   
-  console.log(resultArray);
+//   console.log(resultArray);
   
 
+// **  Merge Two Sorted Arrays**............................................
+
+function mergeSortedArrays(arr1,arr2) {
+       let mergearray=[...arr1,...arr2]
+       let result=[...new Set(mergearray)]
+       console.log (result)
+    }
+    let arr1 =[0,1,2,3,4]
+    let arr2 =[3,4,5,6,7,8,9]
+    mergeSortedArrays(arr1,arr2)
+    
