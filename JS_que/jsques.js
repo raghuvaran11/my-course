945f030483e1e7a629450c9153e57a22211e1a71
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-03-10 19:35:32 +0530

    ass1

--- JS_que/jsques.js
+++ JS_que/jsques.js
@@ -515,3 +515,25 @@
 // } else {
 //     console.log("good night")
 // }
+
+
+
+// 7)Leap Year Check
+// Write a function isLeapYear that takes a year as input and prints "Leap year" if the year is
+// divisible by 4 and not divisible by 100, or if it's divisible by 400. Otherwise, print "Not a leap
+// year."
+
+
+// let year= 2023
+// if (year % 400==0,year % 100 ==0) 
+// {
+//     console.log("this is leap year")
+// }
+//   else if (year % 4==0,year %=! 0)  
+//   {
+//     console.log("this is  leap year")
+// } 
+//   else 
+//   {
+//     console.log("not leap year")
+// }
